fdef1e117df95b8a2c8dcb712abbd557405aa6b6
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-07-01 20:28:34 +0700

    Add files via upload

--- install-passwall.sh
+++ install-passwall.sh
@@ -10,6 +10,7 @@ fi
 mkdir -p /usr/share/xray/
 mkdir -p /usr/share/v2ray/
 wget --no-check-certificate "https://github.com/wegare123/Passwall-Openclash/blob/main/kcptun-client?raw=true" -O /usr/bin/kcptun-client
+wget --no-check-certificate "https://github.com/wegare123/Passwall-Openclash/blob/main/brook.ipk?raw=true" -O /root/brook.ipk
 wget --no-check-certificate "https://github.com/wegare123/Passwall-Openclash/blob/main/passwall.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && opkg install --force-depends *.ipk && rm -rf ekstrak.zip
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/v2ray" -O /usr/bin/v2ray
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/v2ctl" -O /usr/bin/v2ctl
@@ -18,7 +19,6 @@ wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/mai
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geosite.dat" -O /usr/share/xray/geosite.dat
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geoip.dat" -O /usr/share/v2ray/geoip.dat
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geosite.dat" -O /usr/share/v2ray/geosite.dat
-wget --no-check-certificate "https://github.com/wegare123/Passwall-Openclash/blob/main/brook.ipk?raw=true" -O /root/brook.ipk
 mv ~/xray-plugin /usr/bin/
 chmod +x /usr/bin/xray-plugin
 chmod +x /usr/bin/v2ray
