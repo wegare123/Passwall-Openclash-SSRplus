f927becd54b043fd4dd5bd0d9c05805b09f1ff0c
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-07-01 18:24:15 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo > /dev/null
 else
 opkg remove dnsmasq && opkg install dnsmasq-full
 fi
-wget --no-check-certificate "https://github.com/wegare123/Passwall-Openclash/blob/main/passwall-openclash.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && opkg install --force-depends *.ipk && rm -rf *.ipk && rm -rf ekstrak.zip
+wget --no-check-certificate "https://github.com/wegare123/Passwall-Openclash/blob/main/passwall-openclash.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && opkg install --force-depends *.ipk && rm -rf ekstrak.zip
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/v2ray" -O /usr/bin/v2ray
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/v2ctl" -O /usr/bin/v2ctl
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geoip.dat" -O /usr/bin/geoip.dat
@@ -21,6 +21,7 @@ chmod +x /usr/bin/v2ctl
 chmod +x /usr/bin/geoip.dat
 chmod +x /usr/bin/geosite.dat
 chmod +x /usr/bin/kcptun-client
+rm -rf *.ipk 
 rm -r ~/install.sh
 echo "install selesai"
 
