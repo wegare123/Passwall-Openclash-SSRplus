49e470d6efdda4143cf82e618f9e37a61553ca82
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-07-01 14:39:22 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ else
 opkg remove dnsmasq && opkg install dnsmasq-full
 fi
 wget --no-check-certificate "https://github.com/wegare123/Passwall-Openclash/blob/main/passwall-openclash.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && opkg install --force-depends *.ipk && rm -rf *.ipk && rm -rf ekstrak.zip
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/v2ray" -O /usr/bin/v2ray
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/v2ctl" -O /usr/bin/v2ctl
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geoip.dat" -O /usr/bin/geoip.dat
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geosite.dat" -O /usr/bin/geosite.dat
 mv ~/xray-plugin /usr/bin/
-mv ~/v2ray /usr/bin/
-mv ~/v2ctl /usr/bin/
-mv ~/geoip.dat /usr/bin/
-mv ~/geosite.dat /usr/bin/
 chmod +x /usr/bin/xray-plugin
 chmod +x /usr/bin/v2ray
 chmod +x /usr/bin/v2ctl
