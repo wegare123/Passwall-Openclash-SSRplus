9759e05eeda51cfd7155220860b54e7514092dc1
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-07-01 18:08:00 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -12,12 +12,15 @@ wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/mai
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/v2ctl" -O /usr/bin/v2ctl
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geoip.dat" -O /usr/bin/geoip.dat
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/vmt/main/geosite.dat" -O /usr/bin/geosite.dat
+wget --no-check-certificate "https://github.com/wegare123/Passwall-Openclash/blob/main/kcptun-client?raw=true" -O /usr/bin/kcptun-client
+wget --no-check-certificate "https://github.com/wegare123/Passwall-Openclash/blob/main/brook.ipk?raw=true" -O /root/brook.ipk
 mv ~/xray-plugin /usr/bin/
 chmod +x /usr/bin/xray-plugin
 chmod +x /usr/bin/v2ray
 chmod +x /usr/bin/v2ctl
 chmod +x /usr/bin/geoip.dat
 chmod +x /usr/bin/geosite.dat
+chmod +x /usr/bin/kcptun-client
 rm -r ~/install.sh
 echo "install selesai"
 
